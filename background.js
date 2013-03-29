bffa55742fba06e0871f9ea4008f999f17b4c1e9
Jared Tyler <tyler.jared@gmail.com>
2013-03-29 00:38:04 -0400

    Moving RSS checking into the interval handler and properly updating the icon based on results.  Should now work properly. (We'll find out when there's a new update!)

--- background.js
+++ background.js
@@ -3,99 +3,34 @@
 	var checkInterval = null,
 		intervalLength = 5000;
 	
+	var icons = {
+		idle: 'mspa_face.gif',
+		error: 'mspa_reader.gif',
+		updates: 'whatpumpkin.gif'
+	};
+	
 	if (typeof(localStorage['interval_length']) != 'undefined') {
 		intervalLength = localStorage['interval_length'];
 	}
 	
-	var tempCurrentIcon = 'mspa_face.gif';
 	var gotoMspa = function()
 	{
-		// TODO: does not currently go to mspa, instead changes button icon
-		
-		var currentIcon = tempCurrentIcon;
-		switch (currentIcon) {
-			case 'mspa_face.gif':
-				tempCurrentIcon = 'mspa_reader.gif';
-				break;
-			case 'mspa_reader.gif':
-				tempCurrentIcon = 'whatpumpkin.gif';
-				break;
-			case 'whatpumpkin.gif':
-			default:
-				tempCurrentIcon = 'mspa_face.gif';
-				break;
-		}
-		
-		chrome.browserAction.setIcon({path: tempCurrentIcon});
-		
-		// TODO: testing rss reading, this should be moved into the interval checker
-		var lastPageRead = null;
+		var lastPageRead = "http://mspaintadventures.com";
 		if (typeof(localStorage['last_page_read']) != 'undefined') {
 			lastPageRead = localStorage['last_page_read'];
 		}
 		
-		var checkRssFeed = function()
-		{
-			var feedUri = "http://mspaintadventures.com/rss/rss.xml",
-				request = new XMLHttpRequest();
-			
-			request.onload = function()
-			{
-				var xml = request.responseXML;
-				if (!xml) {
-					console.log('invalid rss feed received.');
-					return;
-				}
-				
-				var pages = xml.getElementsByTagName('item');
-				var count = Math.min(pages.length, 40);
-				var item, guid;
-				var unreadPagesCount = 0;
-				for (var i = 0; i < count; i += 1) {
-					item = pages.item(i);
-					
-					guid = item.getElementsByTagName('guid')[0];
-					if (guid) {
-						guid = guid.textContent;
-					}
-					
-					if (guid == lastPageRead) {
-						break;
-					}
-					
-					unreadPagesCount++;
-				}
-				
-				var unreadPagesText = unreadPagesCount + '';
-				if (unreadPagesCount == 40) {
-					unreadPagesText += '+';
-				}
-				
-				chrome.browserAction.setBadgeBackgroundColor({color: '#00AA00'});
-				chrome.browserAction.setBadgeText({text: unreadPagesText});
-			};
-			
-			request.onerror = function()
-			{
-				console.log('something went wrong, brotha.')
-				return;
-			};
-			
-			request.open('GET', feedUri, true);
-			request.send(null);
-			
-			return;
-		};
+		var latestUpdate = "http://mspaintadventures.com";
+		if (typeof(localStorage['latest_update']) != 'undefined') {
+			latestUpdate = localStorage['latest_update'];
+		}
 		
-		chrome.browserAction.getBadgeText({}, function(result) {
-			if (result != '') {
-				chrome.browserAction.setBadgeText({text: ''});
-			} else {
-				checkRssFeed();
-			}
-		});
-		
+		chrome.browserAction.setIcon({path: icons.idle});
+		chrome.browserAction.setBadgeText({text: ''});
 		chrome.tabs.create({url: lastPageRead});
+		
+		localStorage['last_page_read'] = latestUpdate;
+		delete localStorage['latest_update'];
 		
 		return;
 	};
@@ -107,11 +42,69 @@
 			lastPageRead = localStorage['last_page_read'];
 		}
 		
-		/* TODO:
-		 *  check RSS
-		 *  change icon
-		 *  set count on button
-		 */
+		var feedUri = "http://mspaintadventures.com/rss/rss.xml",
+			request = new XMLHttpRequest();
+		
+		request.onload = function()
+		{
+			var xml = request.responseXML;
+			if (!xml) {
+				console.log('invalid rss feed received.');
+				return;
+			}
+			
+			var pages = xml.getElementsByTagName('item');
+			var count = Math.min(pages.length, 40);
+			var item, guid, latestUpdate = null;
+			var unreadPagesCount = 0;
+			for (var i = 0; i < count; i += 1) {
+				item = pages.item(i);
+				
+				guid = item.getElementsByTagName('guid')[0];
+				if (guid) {
+					guid = guid.textContent;
+				}
+				
+				if (guid == lastPageRead) {
+					break;
+				}
+				
+				if (latestUpdate == null) {
+					latestUpdate = guid;
+				}
+				
+				unreadPagesCount++;
+			}
+			
+			if (unreadPagesCount < 1) {
+				chrome.browserAction.setIcon({path: icons.idle});
+				return;
+			}
+			
+			var unreadPagesText = unreadPagesCount + '';
+			if (unreadPagesCount == 40) {
+				unreadPagesText += '+';
+			}
+			
+			localStorage['latest_update'] = latestUpdate;
+			chrome.browserAction.setIcon({path: icons.updates});
+			chrome.browserAction.setBadgeBackgroundColor({color: '#00AA00'});
+			chrome.browserAction.setBadgeText({text: unreadPagesText});
+			
+			return;
+		};
+		
+		request.onerror = function()
+		{
+			console.log('something went wrong, brotha.')
+			chrome.browserAction.setIcon({path: icons.error});
+			return;
+		};
+		
+		request.open('GET', feedUri, true);
+		request.send(null);
+		
+		return;
 	};
 	
 	checkInterval = setInterval(checkForUpdates, intervalLength);
