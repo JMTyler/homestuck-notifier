a000cff1f63051534ed46c29832c3a4436674015
Jared Tyler <tyler.jared@gmail.com>
2013-03-29 00:21:20 -0400

    Button now does more things it's not supposed to: - reads RSS feed from MSPA - counts number of unread pages up to 40 - opens last read page in a new tab (okay, it is supposed to do this one)

--- background.js
+++ background.js
@@ -10,7 +10,7 @@
 	var tempCurrentIcon = 'mspa_face.gif';
 	var gotoMspa = function()
 	{
-		// does not currently go to mspa, instead changes button icon
+		// TODO: does not currently go to mspa, instead changes button icon
 		
 		var currentIcon = tempCurrentIcon;
 		switch (currentIcon) {
@@ -28,11 +28,85 @@
 		
 		chrome.browserAction.setIcon({path: tempCurrentIcon});
 		
+		// TODO: testing rss reading, this should be moved into the interval checker
+		var lastPageRead = null;
+		if (typeof(localStorage['last_page_read']) != 'undefined') {
+			lastPageRead = localStorage['last_page_read'];
+		}
+		
+		var checkRssFeed = function()
+		{
+			var feedUri = "http://mspaintadventures.com/rss/rss.xml",
+				request = new XMLHttpRequest();
+			
+			request.onload = function()
+			{
+				var xml = request.responseXML;
+				if (!xml) {
+					console.log('invalid rss feed received.');
+					return;
+				}
+				
+				var pages = xml.getElementsByTagName('item');
+				var count = Math.min(pages.length, 40);
+				var item, guid;
+				var unreadPagesCount = 0;
+				for (var i = 0; i < count; i += 1) {
+					item = pages.item(i);
+					
+					guid = item.getElementsByTagName('guid')[0];
+					if (guid) {
+						guid = guid.textContent;
+					}
+					
+					if (guid == lastPageRead) {
+						break;
+					}
+					
+					unreadPagesCount++;
+				}
+				
+				var unreadPagesText = unreadPagesCount + '';
+				if (unreadPagesCount == 40) {
+					unreadPagesText += '+';
+				}
+				
+				chrome.browserAction.setBadgeBackgroundColor({color: '#00AA00'});
+				chrome.browserAction.setBadgeText({text: unreadPagesText});
+			};
+			
+			request.onerror = function()
+			{
+				console.log('something went wrong, brotha.')
+				return;
+			};
+			
+			request.open('GET', feedUri, true);
+			request.send(null);
+			
+			return;
+		};
+		
+		chrome.browserAction.getBadgeText({}, function(result) {
+			if (result != '') {
+				chrome.browserAction.setBadgeText({text: ''});
+			} else {
+				checkRssFeed();
+			}
+		});
+		
+		chrome.tabs.create({url: lastPageRead});
+		
 		return;
 	};
 	
 	var checkForUpdates = function()
 	{
+		var lastPageRead = null;
+		if (typeof(localStorage['last_page_read']) != 'undefined') {
+			lastPageRead = localStorage['last_page_read'];
+		}
+		
 		/* TODO:
 		 *  check RSS
 		 *  change icon
