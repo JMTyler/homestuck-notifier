9f9d5c05756bbdf85791887aa5ba30b0b603b18d
Jared Tyler <jared@mappedin.ca>
2019-05-12 00:33:13 -0400

    Cleanup.

--- chrome/version.js
+++ chrome/version.js
@@ -1,15 +1,13 @@
 
 // TODO: Really have to go through this and comment... it's confusing as heck.  But it works!
 
-var jmtyler = jmtyler || {};
-jmtyler.version = (function()
-{
-	var _init = function()
-	{
-		_addUpdate({
+const jmtyler = jmtyler || {};
+jmtyler.version = (() => {
+	const init = () => {
+		addUpdate({
 			from: '1.0.1',
 			to:   '1.1.0',
-			with: function() {
+			with: () => {
 				if (typeof localStorage['check_frequency'] != 'undefined') {
 					jmtyler.settings.set('check_frequency', JSON.parse(localStorage['check_frequency']));
 					delete localStorage['check_frequency'];
@@ -29,42 +27,39 @@ jmtyler.version = (function()
 					jmtyler.memory.set('latest_update', localStorage['latest_update']);
 					delete localStorage['latest_update'];
 				}
-			}
+			},
 		});
 
-		_addUpdate({
+		addUpdate({
 			from: '1.2.1',
 			to:   '1.3.0',
-			with: function() {
+			with: () => {
 				jmtyler.memory.clear('is_gigapause_over');
-			}
+			},
 		});
 
-		_addUpdate({
+		addUpdate({
 			from: '1.4.2',
 			to:   '1.5.0',
-			with: function() {
+			with: () => {
 				// TODO: Delete `check_frequency` setting.
-				var lastPageRead = jmtyler.memory.get('last_page_read');
-				var matches = lastPageRead.match(/^http:\/\/www\.mspaintadventures\.com\/?.*\?s=6&p=(\d+)/);
+				const lastPageRead = jmtyler.memory.get('last_page_read');
+				const matches = lastPageRead.match(/^http:\/\/www\.mspaintadventures\.com\/?.*\?s=6&p=(\d+)/);
 				if (matches === null) {
 					return;
 				}
 
-				var mspaPageId = parseInt(matches[1], 10);
-				var homestuckPageId = mspaPageId - 1900;
+				const mspaPageId = parseInt(matches[1], 10);
+				const homestuckPageId = mspaPageId - 1900;
 				jmtyler.memory.set('last_page_read', 'https://www.homestuck.com/story/' + homestuckPageId);
-			}
+			},
 		});
-
-		return;
 	};
 
-	var _updates = {};
-	var _addUpdate = function(details)
-	{
-		_updates[details.to] = {
-			run: function() {
+	const updates = {};
+	const addUpdate = (details) => {
+		updates[details.to] = {
+			run: () => {
 				jmtyler.log('      updating to version [' + details.to + ']', localStorage);
 
 				details.with();
@@ -72,46 +67,44 @@ jmtyler.version = (function()
 
 				jmtyler.log('      finished updating', localStorage);
 			},
-			next: function() {
+			next: () => {
 				return false;
-			}
+			},
 		};
 
-		if (typeof _updates[details.from] == 'undefined') {
-			_updates[details.from] = {
-				run: function() {
+		if (typeof updates[details.from] == 'undefined') {
+			updates[details.from] = {
+				run: () => {
 					jmtyler.log('      no changes for version [' + details.from + ']');
 					jmtyler.memory.set('version', details.from);
-				}
+				},
 			};
 		}
 
-		_updates[details.from].next = function() {
-			return _updates[details.to];
+		updates[details.from].next = () => {
+			return updates[details.to];
 		};
 	};
-	var _getUpdate = function(version)
-	{
-		if (typeof _updates[version] == 'undefined') {
+
+	const getUpdate = (version) => {
+		if (typeof updates[version] == 'undefined') {
 			return false;
 		}
 
-		return _updates[version];
+		return updates[version];
 	};
 
-	_init();
+	init();
 
 	return {
-		install: function(version)
-		{
+		install(version) {
 			jmtyler.log('fresh install', version);
 			jmtyler.memory.set('version', version);
 		},
-		update: function(fromVersion, toVersion)
-		{
+		update(fromVersion, toVersion) {
 			jmtyler.log('    updating extension...', fromVersion, toVersion);
 
-			var update = _getUpdate(fromVersion);
+			let update = getUpdate(fromVersion);
 			if (update) {
 				while (update = update.next()) {
 					update.run();
@@ -121,13 +114,11 @@ jmtyler.version = (function()
 
 			jmtyler.log('    finished updating extension');
 		},
-		isInstalled: function(version)
-		{
+		isInstalled(version) {
 			if (jmtyler.memory.get('version') == version) {
 				return true;
 			}
-
 			return false;
-		}
+		},
 	};
 })();
